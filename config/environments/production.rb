5d7ddf4da6b29daf1c89d133c450fc71003311b2
Paul Gruson <p.gruson@gmail.com>
2016-09-15 19:26:37 +0200

    domaine

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
     password: "CMcLwOdQPn7bh80t"
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => "http://plannings.paulgruson.fr" }
+  config.action_mailer.default_url_options = { :host => "http://plannings.eglisedeblois.fr" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
