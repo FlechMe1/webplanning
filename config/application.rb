f5f6ad914515cebe6d3608f6eed615cacf1358e1
Paul Gruson <p.gruson@gmail.com>
2018-01-08 12:01:57 +0100

    assets pipe

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,8 @@ module Webplanning
       Devise::Mailer.layout "mailer" # email.haml or email.erb
     end
 
+    config.assets.enabled = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
