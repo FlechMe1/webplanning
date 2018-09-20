2addca2df2f195da531a29dbc8dcf24167a1c3fb
Paul Gruson <p.gruson@gmail.com>
2018-09-20 17:11:31 +0200

    schedule timezone

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -8,6 +8,16 @@ class SchedulesController < ApplicationController
         @events = Event.get_personnal_events(@user.id, DateTime.now)
         @cal = Icalendar::Calendar.new
         @cal.x_wr_calname = 'EGLISE DE BLOIS - Mon Planning'
+        @cal.timezone do |t|
+          t.tzid = "Europe/Paris"
+          t.daylight do |d|
+            d.tzoffsetfrom = "-0100"
+            d.tzoffsetto   = "+0200"
+            d.tzname       = "CDT"
+            d.dtstart      = "19710101T030000"
+            d.rrule        = "FREQ=YEARLY;WKST=MO;INTERVAL=1;BYMONTH=10;BYDAY=-1SU"
+          end
+        end
         @events.each do |ev|
           @cal.event do |e|
             e.dtstart     = ev.begin_at.utc
